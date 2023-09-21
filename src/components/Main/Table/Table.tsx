77425d488f124a322354fdbe12f1a2c5c610682e
Vinicius Bussolo <vinicius.buss13@gmail.com>
2023-09-21 14:17:42 -0300

    :sparkles: feat: changed component table

--- src/components/Main/Table/Table.tsx
+++ src/components/Main/Table/Table.tsx
@@ -1,10 +1,13 @@
 'use client'
+
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 
 import Avatar from '@/assets/avatar-admin.png'
 import supabase from '@/services/supabase'
 
-import { useEffect, useState } from 'react'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
+import { table } from 'console'
 
 interface Customers {
   created_at: string
@@ -36,38 +39,36 @@ export function Table() {
 
   return (
     <>
-      <table className="w-full mt-2 ml-5 border-separate border-spacing-y-11">
-        <tbody>
-          {customers?.map((item) => {
-            return (
-              <tr key={item.id}>
-                <td>
-                  <Image src={Avatar} alt="Icon Avatar" />
-                </td>
-                <td>{item.name}</td>
-                <td>{item.email}</td>
-                <td>{item.phone}</td>
-                {item.status === 'paying' ? (
-                  <td>Adimpente</td>
-                ) : (
-                  <td className="text-red-500">Inadimplente</td>
-                )}
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
-      <div className="flex justify-end gap-4 bg-emerald-600">
-        <button
-          onClick={() => setPage(page - 1)}
-          disabled={page === 1}
-          className="text-red-500"
-        >
-          Previous Page
-        </button>
-        <button onClick={() => setPage(page + 1)} className="text-blue-400">
-          Next Page
-        </button>
+      <div className="h-[37.5rem] overflow-x-hidden flex flex-col justify-between w-full pb-6 pt-9">
+        <table className="max-h-[33.688rem] w-full ml-5 ">
+          <tbody className="divide-y-[50px]">
+            {customers?.map((item) => {
+              return (
+                <tr key={item.id} className="">
+                  <td>
+                    <Image src={Avatar} alt="Icon Avatar" />
+                  </td>
+                  <td>{item.name}</td>
+                  <td>{item.email}</td>
+                  <td>{item.phone}</td>
+                  {item.status === 'paying' ? (
+                    <td>Adimpente</td>
+                  ) : (
+                    <td className="text-red-500">Inadimplente</td>
+                  )}
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+        <div className="flex justify-end gap-4 pr-11">
+          <button onClick={() => setPage(page - 1)} disabled={page === 1}>
+            <ChevronLeft />
+          </button>
+          <button onClick={() => setPage(page + 1)}>
+            <ChevronRight />
+          </button>
+        </div>
       </div>
     </>
   )

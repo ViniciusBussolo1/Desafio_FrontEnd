e2060d7a3cba0a2036744781bedd145839fd7b5d
Vinicius Bussolo <vinicius.buss13@gmail.com>
2023-09-21 11:24:49 -0300

    :heavy_plus_sign: build: added lucide-react

--- src/components/Main/Table/Table.tsx
+++ src/components/Main/Table/Table.tsx
@@ -1,30 +1,74 @@
+'use client'
 import Image from 'next/image'
 
 import Avatar from '@/assets/avatar-admin.png'
+import supabase from '@/services/supabase'
+
+import { useEffect, useState } from 'react'
+
+interface Customers {
+  created_at: string
+  email: string | null
+  id: string
+  name: string | null
+  phone: string | null
+  status: string | null
+  subscription_amount: number | null
+}
 
 export function Table() {
+  const [customers, setCustomers] = useState<Array<Customers> | null>([])
+  const [page, setPage] = useState(1)
+  const [perPage, setPerPage] = useState(5)
+
+  useEffect(() => {
+    const fetchCustomers = async () => {
+      const { data } = await supabase
+        .from('Customers')
+        .select('*')
+        .range((page - 1) * perPage, page * perPage - 1)
+
+      setCustomers(data)
+    }
+
+    fetchCustomers()
+  }, [page, perPage])
+
   return (
-    <table className="w-full mt-2 ml-5 border-separate border-spacing-y-11">
-      <tbody>
-        <tr>
-          <td>
-            <Image src={Avatar} alt="Icon Avatar" />
-          </td>
-          <td>Márcio Braga</td>
-          <td>marcio.braga@minhaempresa.com</td>
-          <td>(21) 98987-7650</td>
-          <td>adimpente</td>
-        </tr>
-        <tr>
-          <td>
-            <Image src={Avatar} alt="Icon Avatar" />
-          </td>
-          <td>Márcio Braga</td>
-          <td>marcio.braga@minhaempresa.com</td>
-          <td>(21) 98987-7650</td>
-          <td>adimpente</td>
-        </tr>
-      </tbody>
-    </table>
+    <>
+      <table className="w-full mt-2 ml-5 border-separate border-spacing-y-11">
+        <tbody>
+          {customers?.map((item) => {
+            return (
+              <tr key={item.id}>
+                <td>
+                  <Image src={Avatar} alt="Icon Avatar" />
+                </td>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+                {item.status === 'paying' ? (
+                  <td>Adimpente</td>
+                ) : (
+                  <td className="text-red-500">Inadimplente</td>
+                )}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <div className="flex justify-end gap-4 bg-emerald-600">
+        <button
+          onClick={() => setPage(page - 1)}
+          disabled={page === 1}
+          className="text-red-500"
+        >
+          Previous Page
+        </button>
+        <button onClick={() => setPage(page + 1)} className="text-blue-400">
+          Next Page
+        </button>
+      </div>
+    </>
   )
 }

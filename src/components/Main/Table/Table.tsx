8c7e63a4556c316765fc0e88fa0c50e858beca05
Vinicius Bussolo <vinicius.buss13@gmail.com>
2023-09-25 13:08:03 -0300

    :sparkles: feat: added customer search results

--- src/components/Main/Table/Table.tsx
+++ src/components/Main/Table/Table.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 
 import Avatar from '@/assets/avatar-admin.png'
 import supabase from '@/services/supabase'
 
 import { ChevronLeft, ChevronRight } from 'lucide-react'
+import { SearchBlocosContext } from '@/context/SearchCustomersContext'
 
 interface Customers {
   created_at: string
@@ -22,6 +23,8 @@ export function Table() {
   const [customers, setCustomers] = useState<Array<Customers> | null>([])
   const [page, setPage] = useState(1)
   const [perPage, setPerPage] = useState(5)
+
+  const { results } = useContext(SearchBlocosContext)
 
   useEffect(() => {
     const fetchCustomers = async () => {
@@ -41,23 +44,55 @@ export function Table() {
       <div className="h-[37.5rem] overflow-x-hidden flex flex-col justify-between w-full pb-6 pt-9">
         <table className="max-h-[33.688rem] w-full ml-5 ">
           <tbody className="divide-y-[50px]">
-            {customers?.map((item) => {
-              return (
-                <tr key={item.id} className="">
-                  <td>
-                    <Image src={Avatar} alt="Icon Avatar" />
-                  </td>
-                  <td>{item.name}</td>
-                  <td>{item.email}</td>
-                  <td>{item.phone}</td>
-                  {item.status === 'paying' ? (
-                    <td>Adimpente</td>
-                  ) : (
-                    <td className="text-red-500">Inadimplente</td>
-                  )}
-                </tr>
-              )
-            })}
+            {results?.length !== 8 ? (
+              <>
+                {results?.length === 0 ? (
+                  <tr>
+                    <td>Nao Foi encontrado nenhum cliente</td>
+                  </tr>
+                ) : (
+                  <>
+                    {results?.map((item) => {
+                      return (
+                        <tr key={item.id} className="">
+                          <td>
+                            <Image src={Avatar} alt="Icon Avatar" />
+                          </td>
+                          <td>{item.name}</td>
+                          <td>{item.email}</td>
+                          <td>{item.phone}</td>
+                          {item.status === 'paying' ? (
+                            <td>Adimpente</td>
+                          ) : (
+                            <td className="text-red-500">Inadimplente</td>
+                          )}
+                        </tr>
+                      )
+                    })}
+                  </>
+                )}
+              </>
+            ) : (
+              <>
+                {customers?.map((item) => {
+                  return (
+                    <tr key={item.id} className="">
+                      <td>
+                        <Image src={Avatar} alt="Icon Avatar" />
+                      </td>
+                      <td>{item.name}</td>
+                      <td>{item.email}</td>
+                      <td>{item.phone}</td>
+                      {item.status === 'paying' ? (
+                        <td>Adimpente</td>
+                      ) : (
+                        <td className="text-red-500">Inadimplente</td>
+                      )}
+                    </tr>
+                  )
+                })}
+              </>
+            )}
           </tbody>
         </table>
         <div className="flex justify-end gap-4 pr-11">
